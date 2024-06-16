7e49dcad395267edfa67fd7f8c6c22141220facd
therahul94 <therahulkurmalkar@gmail.com>
2024-06-16 17:28:05 +0530

    init-161

--- week-7/week7/src/Components/Githubcard.jsx
+++ week-7/week7/src/Components/Githubcard.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
-
-export default function Githubcard(){
+import React, { useState } from "react";
+import "./Githubcard.css";
+import axios from "axios";
+export default function Githubcard() {
+  const [username, setusername] = useState("");
+  const [githubInfo, setGithubInfo] = useState("");
+  async function getGithubInfo() {
+    try {
+      const res = await axios.get(`https://api.github.com/users/${username}`);
+      if (res.status === 200) {
+        setGithubInfo(JSON.stringify(res.data));
+      }
+    } catch (error) {
+      console.log(error);
+      return alert("something went wrong!");
+    }
+  }
   return (
-    <div>Githubcard</div>
-  )
+    <div>
+      <div className="main-container">
+        <input
+          type="text"
+          placeholder="GitHub username"
+          className="github-input"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        />
+        <button className="github-find-btn" onClick={getGithubInfo}>
+          Find
+        </button>
+      </div>
+      {githubInfo ? <Showgithubcard data={githubInfo} /> : <></>}
+    </div>
+  );
+}
+
+function Showgithubcard({ data }) {
+  const val = JSON.parse(data);
+  return (
+    <div className="Showgithubcard-main-container">
+      <div className="Showgithubcard-container">
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={val?.avatar_url}
+            alt="no-image"
+            style={{ borderRadius: "50%", width: "150px" }}
+          />
+          <p>
+            <b style={{ fontSize: "30px", fontFamily: "system-ui" }}>
+              {val?.name}
+            </b>
+            <div style={{ fontFamily: "system-ui" }}>{val?.login}</div>
+          </p>
+        </div>
+        <p>
+          <div>
+            <label>Bio:</label>
+            <div>{val?.bio}</div>
+          </div>
+          <p className="row">
+            <div className="col-md-6">
+                <label></label>
+            </div>
+            <div className="col-md-6"></div>
+            <div className="col-md-6"></div>
+          </p>
+        </p>
+      </div>
+    </div>
+  );
 }
